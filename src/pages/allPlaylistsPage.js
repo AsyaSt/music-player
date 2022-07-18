33b47fff772f0bbd232a71e0d0c3b8cf7b6ea6f9
Stepanova Asya <asenkabsq@gmail.com>
2022-07-19 00:58:34 +0300

    => all Playlists page

---
+++ src/pages/allPlaylistsPage.js
@@ -0,0 +1,21 @@
+import { connect } from "react-redux";
+import { PlaylistsAll } from "../components/Playlist";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCompactDisc} from "@fortawesome/free-solid-svg-icons";
+
+
+export const AllPlaylistsPage = () => {
+    return(
+        <>
+        <div className="d-flex justify-content-between align-items-center py-3">
+            <h3 className="text-uppercase"> <FontAwesomeIcon icon={faCompactDisc} className="me-2"/>Playlists</h3>
+        </div>
+        <CAllPlaylists/>
+        </>
+    )
+}
+
+
+
+
+export const CAllPlaylists = connect(state => ({playlists: state.promise.allPlaylists?.payload?.playlists?.data || []}), )(PlaylistsAll);
